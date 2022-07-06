358fcd054ed196cd135e47820df88e7b24f69efc
Yifan_Yang <yyangdev@gmail.com>
2022-07-06 14:55:19 -0700

    feat: get genome type, and organized utils
    moved utility functions to utils, and added a feature to acquire genome from hgdownload.cse.ucsc.edu. It now allows mm10, mm9m hg19 and hg39.

--- src/epivizquindex/EpivizQuindex.py
+++ src/epivizquindex/EpivizQuindex.py
@@ -1,11 +1,12 @@
 import os
 from hilbertcurve.hilbertcurve import HilbertCurve
-from QuadTree import Index
+from epivizquindex.QuadTree import Index
 import pickle
 import sys
 import math
 import json
 import pandas
+from epivizquindex.utils import hcoords, range2bbox
 from epivizFileParser import BigWig
 from epivizFileParser.utils import toDataFrame
 import struct
@@ -14,101 +15,6 @@ __author__ = "Jayaram Kancherla"
 __copyright__ = "Jayaram Kancherla"
 __license__ = "mit"
 
-def hcoords(x, chromLength, dims = 2):
-    '''
-    Returns hilbert space coordinate of the input query.
-
-            Parameters:
-            - **x (int)**:            A integer representing the query location in a chromosome.
-            - **chromLength (int)**:  The total length of the chromosome.
-            - **dims (int)**:         dimension of the hilbert space, by default we use 2d hilbert space.
-
-            Returns:
-            - **x (int)**:        x coordinate in hilbert space.
-            - **y (int)**:        y coordinate in hilbert space.
-            - **hlevel (int)**:   level of the hilbert space.
-    '''
-    hlevel = math.ceil(math.log2(chromLength)/dims)
-    # print("hlevel, ", hlevel)
-    hilbert_curve = HilbertCurve(hlevel, dims)
-    [x,y] = hilbert_curve.points_from_distances([x])[0]
-    return x, y, hlevel
-
-# query : dictionary with 2 items, start and end
-def range2bbox(hlevel, query, dims = 2, margin = 0):
-    '''
-    Convert the input query chromosome range to query bounding box in hilbert space.
-
-            Parameters:
-            - **hlevel (int)**:       level of the hilbert space, calculated by the size of the chromosome.
-            - **query (dict)**:       dictionary of a query range, with "start" and "end" representing the query range in a chromosome.
-            - **dims (int)**:         dimension of the hilbert space, by default we use 2d hilbert space.
-            - **margin (int)**:       margin of the query box in hilbert space.
-
-            Returns:
-            - **bbox (tuple)**: 4 integers representing the lower left and top right coordinate. The order is lower left x, y, then top right x, y.
-    '''
-    # now = time.time()
-    # query = {
-    #     "start": 0,
-    #     "end":  127074
-    #     }
-    hilbert_curve = HilbertCurve(hlevel, dims)
-    inc = 0
-    # ite = 0
-    start = query["start"]+1
-    points = []
-    if start%4 == 1:
-        points.append(start)
-        start += 1
-    if start%4 == 2:
-        points.append(start)
-        start += 1
-    if start%4 == 3: 
-        points.append(start)
-        start += 1 
-    points.append(start)
-
-    # assume at this ppoint, start is always at the end of a level 0
-    while start < query["end"] + 1:
-        # ite += 1
-        # print(inc)
-        # locate the proper power incrementer
-        # the incrementor indicates the maximum power of 4
-        while start % (4**(inc+1)) == 0:
-            inc += 1
-        while inc >= 0:
-            # to get min x, min y, max x, max y, it is necessary to
-            # locate the diagnol coordinates.
-            # the 3rd point of the thrid sub-quadrons is always diagnol
-            # to the starting point.
-            if start + (4**inc) <= query["end"] + 1:
-                points.append(start + 1)
-                displacement = 0
-                for x in range(inc - 1, -1, -1):
-                    # the following lines are equivalent, and does not
-                    # improve any speed
-                    # displacement = displacement | (0b01 << (2 * x))
-                    displacement += 2 * 4 ** x
-                points.append(start + displacement + 1)
-                start += 4 ** inc
-                break
-            else:
-                inc = inc - 1
-
-    # print(points)
-    hillcorX = []
-    hillcorY = []
-    # for point in points:
-    for h_point in hilbert_curve.points_from_distances(points):
-        [x, y] = h_point
-        # print(x, y, point)
-        hillcorX.append(x)
-        hillcorY.append(y)
-    bbox = (min(hillcorX) - margin, min(hillcorY) - margin, max(hillcorX) + margin, max(hillcorY) + margin)
-    # print(bbox)
-    # print(time.time() - now)
-    return bbox
 
 class EpivizQuindex(object):
 
