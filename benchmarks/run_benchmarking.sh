00da9c53f167245fb4b70b078037d7cc21bc8b7e
Yifan_Yang <yyangdev@gmail.com>
2022-08-12 17:12:46 -0700

    benchmark: option for number of files
    added number of files option for benchmarking a large database.

--- benchmarks/run_benchmarking.sh
+++ benchmarks/run_benchmarking.sh
@@ -1,4 +1,5 @@
-python benchmarking.py --query_range 50 > 50_
+python benchmarking.py --query_range 20 --num_file -1
+
 
 
 for RUNS in 50 500 5000 50000 500000
@@ -6,7 +7,19 @@ do
     for REPEAT in 1 2 3 4 5 6 7 8 9 10
     do
 
-    	python benchmarking.py --query_range $RUNS > ./results/{$RUNS}.{$REPEAT}.txt
+        python benchmarking.py --query_range $RUNS --num_file -1 > ./results/size/{$RUNS}.{$REPEAT}.txt
+
+    done
+done
+
+
+for NUMFILES in 20 40 60 80 100 120 140
+do
+
+    for REPEAT in 1 2 3 4 5 6 7 8 9 10
+    do
+
+        python benchmarking.py --query_range 50000 --num_file $NUMFILES > ./results/num_file/{$NUMFILES}.{$REPEAT}.txt
 
     done
 
